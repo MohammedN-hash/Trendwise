75e1b2f2bb7086a5180e48784a63b61d36d67220
mohammed <I443892>
2023-05-04 12:09:46 +0200

    add home page and initiate analysis page

---
+++ Chart.js
@@ -0,0 +1,110 @@
+// declare a variable to store the data
+let dataset;
+let dataCount;
+// declare a variable to store the chart object
+let chart;
+
+
+async function getData() {
+  try {
+    const response = await fetch('testdata.csv');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.text();
+    dataset = data;
+  } catch (error) {
+    console.error('There was a problem fetching the data:', error);
+  }
+}
+
+
+async function updateChart() {
+  // Get the value of the data
+  const topic = document.getElementById("topic").value;
+  if (chart) {
+    chart.destroy();
+  }
+  if (!dataset) {
+    await getData();
+  }
+
+  // Parse the CSV data using Papa Parse
+  let parsedData = Papa.parse(dataset, { header: true });
+  // Filter each label in a certin topic 
+  let filteredData = parsedData.data.filter(row => row['Query'] === topic);
+  const columnName = 'Label';
+  const negative = filteredData.filter(val => val[columnName] == '0');
+  const natural = filteredData.filter(val => val[columnName] == '2');
+  const positive = filteredData.filter(val => val[columnName] == '4');
+  // Count all filtered labeles
+  const negative_count = negative.length;
+  const natural_count = natural.length;
+  const positive_count = positive.length;
+
+  dataCount = [negative_count, natural_count, positive_count];
+
+  // Create the chart
+  const ctx = document.getElementById('sentiment-chart').getContext('2d');
+
+  chart = new Chart(ctx, {
+    type: 'pie',
+    data: {
+      labels: ["Negative", "Natural", "Positive"],
+      datasets: [{
+        label: 'Person/s',
+        data: dataCount,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.5)',
+          'rgba(54, 162, 235, 0.5)',
+          'rgba(255, 206, 86, 0.5)',
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      title: {
+        display: true,
+        text: 'Social Media Sentiment Analysis'
+      }
+    }
+  });
+
+  // Get sample topics 
+  let Query = parsedData.data.map(row => row['Query']);
+  let uniqueValues = [...new Set(Query)];
+  let uniqueValueCount = uniqueValues.length;
+  let randomValues = uniqueValues.sort(() => Math.random() - 0.5).slice(0, 5);
+  // Show sample topics 
+  const RandomeExamples = document.getElementById("RandomeExamples");
+  RandomeExamples.textContent = 'Randome examples: ' + randomValues.join(", ");
+
+
+  // Get sample articles 
+  const randomIndex = Math.floor(Math.random() * negative.length);
+
+  let RandomePositiveArticles = positive[randomIndex]['Tweet'];
+  let RandomeNegativeArticles = negative[randomIndex]['Tweet'];
+  // Show sample topics 
+  const RandomeArticlesPlaceHolder = document.getElementById("RandomeArticles");
+  RandomeArticlesPlaceHolder.textContent = 'Randome postive: ' + RandomePositiveArticles+'                                            Randome negative '+RandomeNegativeArticles;
+
+} 
+
+
+
+
+// Add an event listener to the text box element
+const textbox = document.getElementById("topic");
+textbox.addEventListener("input", updateChart);
+
+// Call getData initially to get the data at the begining
+getData()
+// Call updateChart initially to display the chart based on the initial value of the text box
+updateChart();
---
+++ README.md
@@ -0,0 +1,23 @@
+# Data-visualisation
+
+This project is a demonstration of sentiment analysis using JavaScript, HTML, and CSS. The code displays a chart that represents the sentiment of different topics. The sentiment of each topic represented by how many people used that topic and what sentiment classification they were classfied
+
+The data for sentiment analysis is obtained from a CSV file using the Papa Parse library. The code then calculates the sentiment score for each topic, and displays the scores as a chart using the Chart.js library.
+
+The chart is interactive, allowing the user to hover over each topic to view its sentiment score. The chart also includes a legend that shows the sentiment range for each color. The code uses HTML and CSS to style the chart and make it visually appealing.
+
+This project is intended as a proof-of-concept for interactive data visualisation. The code can be customized and extended to analyze sentiment for different datasets and topics.
+
+
+``
+MMMMMMMMM             MMMMMMMMMM            MMMMMMMMMMM
+MM      \`\           /'      MM          MM          MM
+MM       \`\         /'       MM         MM            MM
+MM        \`\       /'        MM        MM              MM
+MM         \`\     /'         MM         MM             MM
+MM          \`\   /'          MM          MM           MM
+MM           \`\ /'           MM           MM         MM
+MM            \`V'            MM            MM MMMMMMM
+
+
+``
