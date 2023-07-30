eeac49080ed421a3885e3573e10d7a7764577681
mohammed <moodanwer@gmail.com>
2023-07-30 12:22:03 +0200

    fix category bug

--- latest_news.js
+++ latest_news.js
@@ -1,42 +1,90 @@
-const newsListElement = document.getElementById('news-list');
-const genreSelectElement = document.getElementById('genre');
+// latest_news.js
+document.addEventListener("DOMContentLoaded", function () {
+    const genreSelector = document.getElementById("genre");
+    const newsList = document.getElementById("news-list");
+    let offset = 0;
+    const limit = 10;
 
-async function getLatestNews() {
-    const genre = genreSelectElement.value;
-    const response = await fetch(`http://localhost:8000/latest-news/?genre=${genre}`);
-    const data = await response.json();
-    displayNews(data);
-}
-
-function displayNews(newsData) {
-    newsListElement.innerHTML = '';
-    newsData.forEach(news => {
-        const newsCard = createNewsCard(news);
-        newsListElement.appendChild(newsCard);
+    genreSelector.addEventListener("change", () => {
+        offset = 0; // Reset offset when genre changes
+        fetchAndRenderNews();
     });
-}
 
-function createNewsCard(news) {
-    const newsCard = document.createElement('div');
-    newsCard.classList.add('news-card');
+    window.addEventListener("scroll", () => {
+        // Load more news when user scrolls to the bottom of the page
+        if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+            offset += limit;
+            fetchAndRenderNews(true);
+        }
+    });
 
-    const titleElement = document.createElement('h2');
-    titleElement.textContent = news.title;
+    async function fetchAndRenderNews(loadMore = false) {
+        const selectedCategory = genreSelector.value; // Use selectedCategory instead of selectedGenre
+        const response = await fetch(`http://localhost:8000/latest-news/?category=${selectedCategory}&offset=${offset}&limit=${limit}`); // Use "category" instead of "genre"
+        const articles = await response.json();
 
-    const publishedElement = document.createElement('p');
-    publishedElement.textContent = `Published: ${news.published}`;
+        if (loadMore) {
+            // Append new articles when loading more
+            appendNews(articles);
+        } else {
+            // Replace existing news with new articles
+            newsList.innerHTML = "";
+            renderNews(articles);
+        }
+    }
 
-    const contentElement = document.createElement('p');
-    contentElement.textContent = news.content;
+    function renderNews(articles) {
+        if (articles.length === 0) {
+            newsList.innerHTML = "<p>No news articles found for the selected category.</p>";
+            return;
+        }
 
-    const linkElement = document.createElement('a');
-    linkElement.href = news.link;
-    linkElement.textContent = 'Read more';
+        articles.forEach((article) => {
+            const articleElement = createArticleElement(article);
+            newsList.appendChild(articleElement);
+        });
+    }
 
-    newsCard.appendChild(titleElement);
-    newsCard.appendChild(publishedElement);
-    newsCard.appendChild(contentElement);
-    newsCard.appendChild(linkElement);
+    function appendNews(articles) {
+        if (articles.length === 0) {
+            // No more articles to load
+            return;
+        }
 
-    return newsCard;
-}
+        articles.forEach((article) => {
+            const articleElement = createArticleElement(article);
+            newsList.appendChild(articleElement);
+        });
+    }
+
+    function createArticleElement(article) {
+        const articleElement = document.createElement("div");
+        articleElement.classList.add("article");
+
+        const sourceElement = document.createElement("p");
+        sourceElement.classList.add("source");
+        sourceElement.textContent = `Source: ${article.source}`;
+        articleElement.appendChild(sourceElement);
+
+        const titleElement = document.createElement("a");
+        titleElement.classList.add("title");
+        titleElement.textContent = article.title;
+        titleElement.href = article.link;
+        articleElement.appendChild(titleElement);
+
+        const contentElement = document.createElement("p");
+        contentElement.classList.add("content");
+        contentElement.textContent = article.content;
+        articleElement.appendChild(contentElement);
+
+        const publishedElement = document.createElement("p");
+        publishedElement.classList.add("published");
+        publishedElement.textContent = `Published: ${article.published}`;
+        articleElement.appendChild(publishedElement);
+
+        return articleElement;
+    }
+
+    // Fetch initial news
+    fetchAndRenderNews();
+});
